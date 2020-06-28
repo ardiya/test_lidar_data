26681242bc5cc0a5d59b7f5e66ffa9b828ead5ca
Aditya Ardiya <aditya.ardiya@gmail.com>
2020-06-28 22:44:56 +0900

    Add read lidar data

--- mylib/io.py
+++ mylib/io.py
@@ -1,5 +1,6 @@
 import numpy as np
 from typing import List
+from typing import Tuple
 
 
 def read_trajectories(filename: str, scale_factor: float = 1.0) -> List[np.array]:
@@ -23,6 +24,35 @@ def read_trajectories(filename: str, scale_factor: float = 1.0) -> List[np.array
     return result
 
 
+def read_lidar(filename: str, scale_factor=1.0/1000.0,
+               lidar_range: Tuple[float, float] = [0.01, 10000.0]) -> List[np.array]:
+    """
+    Read lidar sweep data from file and return it as list of nx3x3 numpy array
+    note: the provided test data was weird, lidar_transform is used to convert y to -y
+    """
+    result = list()
+    with open(filename, "r") as fp:
+        while True:
+            line = fp.readline()
+            if line == "":
+                break
+            _, n = [int(x) for x in line.split(",")]
+            lidar_points = []
+            for _ in range(n):
+                line = fp.readline()
+                angle, dist = [float(x) for x in line.split(",")]
+                if not lidar_range[0] <= dist <= lidar_range[1]:
+                    continue
+                x = np.cos(np.deg2rad(angle)) * dist * scale_factor
+                y = np.sin(np.deg2rad(angle)) * dist * scale_factor
+                pt = np.array([[1.0, 0.0, x],
+                               [0.0, 1.0, y],
+                               [0.0, 0.0, 1.0]])
+                lidar_points.append(pt)
+            result.append(np.array(lidar_points))
+    return result
+
+
 if __name__ == "__main__":
     trajectories = read_trajectories("data/FlightPath.csv")
     assert type(trajectories) == list
@@ -30,3 +60,11 @@ if __name__ == "__main__":
     for traj in trajectories:
         assert type(traj) == type(np.array([]))
         assert traj.shape == (3, 3)
+
+    lidar_data = read_lidar("data/LIDARPOINTS.csv")
+    assert type(lidar_data) == list
+    assert len(lidar_data) > 0
+    for points in lidar_data:
+        assert type(points) == type(np.array([]))
+        assert len(points.shape) == 3
+        assert points.shape[1:] == (3, 3)
